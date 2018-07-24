48781e5931e2a579375db02c941737d132246175
matveyvarg <matveyvarg@gmail.com>
2018-07-24 22:29:46 +0700

    no module named

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
@@ -20,7 +20,8 @@ class Common(Configuration):
         # Third party apps
         'rest_framework',            # utilities for rest apis
         'rest_framework.authtoken',  # token authentication
-        'django_filters',            # for filtering rest endpoints
+        'django_filters',            # for filtering rest endpoints,
+        'rest_framework_jwt'
         'rest_framework'
         # Your apps
         '{{cookiecutter.app_name}}.users',
